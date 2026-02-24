e962928a4c1c80603bf31f785ee2726ea6dbc689
ClimateOP <baburaogaonkar906@gmail.com>
2026-02-24 20:02:55 +0530

    add port

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const ytdlp = require('yt-dlp-exec');
 
+const PORT = process.env.PORT || 3000;
+
 const app = express();
 app.use(cors());
 
@@ -40,6 +42,6 @@ app.get('/search', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
