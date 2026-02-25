517390fd253c8f04f286549a40f0a1e4eef4b9ea
ClimateOP <baburaogaonkar906@gmail.com>
2026-02-25 22:26:19 +0530

    render fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const ytdlp = require('yt-dlp-exec');
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 10000;
 
 const app = express();
 app.use(cors());
@@ -42,4 +42,6 @@ app.get('/search', async (req, res) => {
   }
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
